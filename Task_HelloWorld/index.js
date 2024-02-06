31455e771b249983ee719710087859f7ad8af45b
Hemant Navlani <hemant.navlani.0506@gmail.com>
2024-02-06 23:37:10 +0530

    task: Task: Creating a Simple JavaScript Program

--- Task_HelloWorld/index.js
+++ Task_HelloWorld/index.js
@@ -1,26 +1,25 @@
 // Print hello World
-// Write your code here
-
-// Write your code above
-
+console.log("Hello World !!")
 //Declare a variable i and set its value to 5
-// Write your code here
-
-// Write your code above
-
+let i = 5
 //Write the if else statement to console "Number is odd" if the value of i is odd and "Number is even" if the value of i is even.
-// Write your code here
-
-// Write your code above
-
+if (i%2==0){
+    console.log("Number is even")
+}
+else {
+    console.log("Number is odd")
+}
 //Declare a string variable which is always constant and set is value to 'triangle'.
-// Write your code here
-
-// Write your code above
+const string = 'triangle'
 
 //Write a function 'findSide' which takes an argument 'type' and return the number of sides in that shape
-// Write your code here
 
-// Write your code above
+function findSide(type){
+     switch(type){
+        case "Triangle": return 3;
+        case " Square" : return 4;
+        case 'Circle': return Infinity;
+    }
+}
 const side = findSide('Circle');
 console.log('Side', side);
