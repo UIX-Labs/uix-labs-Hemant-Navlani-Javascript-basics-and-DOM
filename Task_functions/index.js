41f455b19b9b9cb5a75cad12a04b9c162c473ac7
Hemant Navlani <hemant.navlani.0506@gmail.com>
2024-02-09 18:08:39 +0530

    task: Implementing JS Functions

--- Task_functions/index.js
+++ Task_functions/index.js
@@ -1,11 +1,15 @@
 //Write code for the 1st task below this
-
-
+function addTWo(num1,num2){
+    return num1+num2;
+}
+console.log(addTWo(3,7))
 //Write your code above this line
-
-
 
 //Write code for the 2nd task below this
 
+function greet(name,greeting='hello'){
+    return `${greeting} ${name}`
+}
+console.log(greet("Hemant"))
 
 //Write your code above this line
