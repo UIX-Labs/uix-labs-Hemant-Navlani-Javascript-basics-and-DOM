c61bd3f7ed1bcf457fcb72bf253a9df778c17c29
Hemant Navlani <hemant.navlani.0506@gmail.com>
2024-02-16 14:35:22 +0530

    task: Project Task-Selecting form element

--- Slambook/index.js
+++ Slambook/index.js
@@ -1,7 +1,8 @@
 let form;
+
 let allSlamArray=[];
 //Write code below to select the form element
-
+form = document.getElementsByTagName("form")
 // Write your code above
 console.log(form,"form");
 
