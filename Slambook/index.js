9aa77b3609ad3428a0b18c72518459de84b5fdb2
Hemant Navlani <hemant.navlani.0506@gmail.com>
2024-02-16 15:06:46 +0530

    task: Project Task-Adding a event listener

--- Slambook/index.js
+++ Slambook/index.js
@@ -2,18 +2,25 @@ let form;
 
 let allSlamArray=[];
 //Write code below to select the form element
-form = document.getElementsByTagName("form")
-// Write your code above
+form = document.querySelector('form')
 console.log(form,"form");
+// Write your code above
 
 // Add the evenlistner below
-
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(document.querySelector('[name="contactName"]').value);
+    console.log(document.querySelector('[name="relationship"]').value);
+    console.log(document.querySelector('[name="likeInMe"]').value);
+    console.log(document.querySelector('[name="hateInMe"]').value);
+    console.log(document.querySelector('[name="deadReaction"]').value);
+    console.log(document.querySelector('[name="firstImpression"]').value);
+    console.log(document.querySelector('[name="beautifulMessage"]').value);
+    console.log(document.querySelector('[name="nickname"]').value);
+    console.log(document.querySelector('[name="songForMe"]').value);
+    console.log(document.querySelector('[name="shareOpinion"]').value);
+})
+ 
 //Write your code above
 
-function showSlamList(){
-//Update the function here
-
-// Write your code above this line
-}
-
-showSlamList();
+        
