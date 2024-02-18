1a7e9abd3256f22f29d271933001a6692789d7d5
Hemant Navlani <hemant.navlani.0506@gmail.com>
2024-02-18 15:06:55 +0530

    task: Project Task-Final Changes

--- Slambook/index.js
+++ Slambook/index.js
@@ -5,14 +5,14 @@ let formValue = {};
 let err ;
 //Write code below to select the form element
 form = document.querySelector('form')
-// console.log(form,"form");
+console.log(form,"form");
 // Write your code above
 
 // Add the evenlistner below
 form.addEventListener('submit',function(e){
     
     formValue={}
-    // allSlamArray=[];
+    allSlamArray=[];
     e.preventDefault();
     if (err){
     err.forEach((element)=>{
@@ -41,22 +41,19 @@ form.addEventListener('submit',function(e){
         tag.after(span);
         return;
     }
-    let flag = false;
-    if (contactName.value === "") {addSpan(contactName); flag = true;}
-    if (relationship.value === "") {addSpan(relationship);flag = true;}
-     if (likeInMe.value === ""){addSpan(likeInMe); flag = true;}
-     if (hateInMe.value === "") {addSpan(hateInMe); flag = true;}
-     if (deadReaction.value === "") {addSpan(deadReaction); flag = true;}
-     if (firstImpression.value === "") {addSpan(firstImpression); flag = true;}
-     if (beautifulMessage.value === "") {addSpan(beautifulMessage);flag = true;}
-     if (nickname.value === "") {addSpan(nickname); flag = true;}
-     if (songForMe.value === ""){ addSpan(songForMe);flag = true;}
-     if (shareOpinion.value === "") {addSpan(shareOpinion); flag = true;}
+    if (contactName.value === "") {addSpan(contactName);}
+    if (relationship.value === "") {addSpan(relationship);}
+     if (likeInMe.value === ""){addSpan(likeInMe); }
+     if (hateInMe.value === "") {addSpan(hateInMe); }
+     if (deadReaction.value === "") {addSpan(deadReaction); }
+     if (firstImpression.value === "") {addSpan(firstImpression); }
+     if (beautifulMessage.value === "") {addSpan(beautifulMessage);}
+     if (nickname.value === "") {addSpan(nickname); }
+     if (songForMe.value === ""){ addSpan(songForMe);}
+     if (shareOpinion.value === "") {addSpan(shareOpinion); }
+
      err = document.querySelectorAll('.error');
-
-     if (flag) return;
-
-
+     console.log(err);
     formValue.contactName = document.querySelector('[name="contactName"]').value
     formValue.relationship = document.querySelector('[name="relationship"]').value
     formValue.likeInMe = document.querySelector('[name="likeInMe"]').value
@@ -68,30 +65,32 @@ form.addEventListener('submit',function(e){
     formValue.songForMe = document.querySelector('[name="songForMe"]').value
     formValue.shareOpinion = document.querySelector('[name="shareOpinion"]').value
     allSlamArray.push(formValue)
-    // console.log(formValue)
-// console.log(allSlamArray)
+    console.log(formValue)
+console.log(allSlamArray)
 showSlamList();
 })
 
 function showSlamList(){
-
     const allSlams = document.querySelector('.allSlams')
+    allSlamArray.forEach((element)=>{
             allSlams.innerHTML += `<div class="card">
-<p>My name in your contact is <span class="answer">${formValue.contactName}</span></p>
-<p>Relationship between us is <span class="answer">${formValue.relationship}</span></p>
-<p>Something you like in me is <span class="answer">${formValue.likeInMe}</span></p>
-<p>Something you hate in me is <span class="answer">${formValue.hateInMe}</span></p>
+<p>My name in your contact is <span class="answer">${element.contactName}</span></p>
+<p>Relationship between us is <span class="answer">${element.relationship}</span></p>
+<p>Something you like in me is <span class="answer">${element.likeInMe}</span></p>
+<p>Something you hate in me is <span class="answer">${element.hateInMe}</span></p>
 <p>If I die what would be your reaction?</p>
-<p><span class="answer">${formValue.deadReaction}</span></p>
+<p><span class="answer">${element.deadReaction}</span></p>
 <p>What did you feel when you first saw me?</p>
-<p><span class="answer">${formValue.firstImpression}</span></p>
+<p><span class="answer">${element.firstImpression}</span></p>
 <p>A beautiful message for me?</p>
-<p><span class="answer">${formValue.beautifulMessage}</span></p>
-<p>A nickname for me is <span class="answer">${formValue.nickname}</span></p>
-<p>A song you want to dedicate to me is <span class="answer">${formValue.songForMe}</span></p>
+<p><span class="answer">${element.beautifulMessage}</span></p>
+<p>A nickname for me is <span class="answer">${element.nickname}</span></p>
+<p>A song you want to dedicate to me is <span class="answer">${element.songForMe}</span></p>
 <p>Can I share your opinion in my status?</p>
-<p><span class="answer">${formValue.shareOpinion}</span></p>
+<p><span class="answer">${element.shareOpinion}</span></p>
 </div>`
+    })
+
 }
 
  
